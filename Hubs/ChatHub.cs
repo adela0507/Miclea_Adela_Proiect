50ca2faad81b8d7e7e4c5551f7ab8a7c2f0cced1
adela miclea <adelamiclea709@yahoo.com>
2024-01-13 16:45:22 +0200

    inceput

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.Authorization;
+
 namespace Miclea_Adela_Proiect.Hubs
 {
+    [Authorize]
+
     public class ChatHub:Hub
     {
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", Context.User.Identity.Name, message);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Miclea_Adela_Proiect.Data;
 using Miclea_Adela_Proiect.Hubs;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,13 @@ builder.Services.AddDbContext<ProductContext>(options =>
 
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<IdentityContext>();
+
 builder.Services.AddSignalR();
+builder.Services.AddDbContext<IdentityContext>(options =>
+
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
@@ -33,13 +40,18 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.MapHub<ChatHub>("/Chat");
+app.MapRazorPages();
+
 
 
 app.Run();
